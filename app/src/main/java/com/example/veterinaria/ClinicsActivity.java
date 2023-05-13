1a6a70b3a0d84c47aee8daf6aaea9fb35f46582c
Samantha <94052042+samanthag97@users.noreply.github.com>
2023-05-13 19:53:30 +0200

    added webview in dashboard
    - google maps still not showing nearby places
    - added webview in dashboard
    - url to google maps (my location) + show veterinary -> ok but not what i want

--- app/src/main/java/com/example/veterinaria/ClinicsActivity.java
+++ app/src/main/java/com/example/veterinaria/ClinicsActivity.java
@@ -1,10 +1,12 @@
 package com.example.veterinaria;
 
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.Manifest;
 import android.widget.Toast;
@@ -15,7 +17,9 @@ import androidx.core.content.ContextCompat;
 
 import com.airbnb.lottie.L;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,6 +30,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.veterinaria.databinding.ActivityClinicsBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
 
 import org.json.JSONException;
@@ -39,6 +45,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ClinicsActivity extends DrawerBaseActivity implements OnMapReadyCallback {
 
@@ -50,10 +58,12 @@ public class ClinicsActivity extends DrawerBaseActivity implements OnMapReadyCal
     Location currentLocation;
     double currentLat;
     double currentLong;
+    private double lat, lng;
 
     private static final int REQUEST_CODE = 101;
     private static final int ZOOM = 10;
-    String veterinario = "veterinarian";
+    String veterinario = "bank";//"veterinary care";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,20 +74,25 @@ public class ClinicsActivity extends DrawerBaseActivity implements OnMapReadyCal
         allocateActivityTitle(getString(R.string.clinics));
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        getCurrentLocation();
+        //getCurrentLocation();
 
-        //still not working but at least it won't crush
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         //part for nearby places
         String url = "https://maps.googleapis.com/maps/place/nearbysearch/json?"+
-                "location=" + currentLat + "," + currentLong +
-                "&radius=1000" + "&types=" + veterinario + "&sensor=true"  +
+                "location=" + lat + "," + lng +
+                "&radius=20000" + "&types=" + veterinario + "&sensor=true"  +
                 "&key=" + getResources().getString(R.string.google_maps_key);
 
         Object dataFetch[] = new Object[2];
         dataFetch[0] = mMap;
         dataFetch[1] = url;
+
+        FetchData fetchData = new FetchData();
+        fetchData.execute(dataFetch);
+
+
 
 
     }
@@ -86,41 +101,50 @@ public class ClinicsActivity extends DrawerBaseActivity implements OnMapReadyCal
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng current = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(current));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, ZOOM));
-
+        getCurrentLocation();
     }
 
     private void getCurrentLocation() {
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             return;
         }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(60000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(5000);
+
+        LocationCallback locationCallback = new LocationCallback();
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback,null);
+
+        Task<Location> task  =fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
+
                 if(location != null){
-                    currentLocation = location;
-                    //Toast.makeText(getApplicationContext(), currentLocation.getLatitude()+""+currentLocation.getLongitude(),Toast.LENGTH_LONG).show();
-                    SupportMapFragment supportMapFragment=(SupportMapFragment) getSupportFragmentManager()
-                            .findFragmentById(R.id.map);
-                    supportMapFragment.getMapAsync(ClinicsActivity.this);
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+                    LatLng latLng = new LatLng(lat, lng);
+                    mMap.addMarker(new MarkerOptions().position(latLng));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                 }
+
             }
         });
 
     }
 
+    @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch (requestCode){
+        switch (REQUEST_CODE){
             case REQUEST_CODE:
                 if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
                     getCurrentLocation();
--- app/src/main/java/com/example/veterinaria/DashboardActivity.java
+++ app/src/main/java/com/example/veterinaria/DashboardActivity.java
@@ -2,12 +2,21 @@ package com.example.veterinaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.veterinaria.databinding.ActivityDashboardBinding;
 
 public class DashboardActivity extends DrawerBaseActivity {
 
     ActivityDashboardBinding activityDashboardBinding;
+
+    //piccolo escamotage per vedere cliniche in zona
+    String url = "https://www.google.it/maps/search/veterinary/@45.9845092,13.4187732,10z";
+
+    //enci not working
+    //"https://www.enci.it/enci/news";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,14 @@ public class DashboardActivity extends DrawerBaseActivity {
         activityDashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
         setContentView(activityDashboardBinding.getRoot());
         allocateActivityTitle(getString(R.string.dashboard));
+
+        WebView webView = (WebView) findViewById(R.id.web_view_dashboard);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient());
 
     }
 
